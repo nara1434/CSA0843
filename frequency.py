1cf181b20aec3b01dc7bf8e28ebcebac7d21b64e
nara1434 <112242605+nara1434@users.noreply.github.com>
2022-10-01 09:14:56 +0530

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,12 @@
+n=int(input("enter the size:"))
+list=[]
+K={}
+for i in range(n):
+    b=int(input("enter the elements:"))
+    list.append(b)
+for i in list:30
+    if i in K:
+        K[i]+=1
+    else:
+        K[i]=1
+print(K)        
